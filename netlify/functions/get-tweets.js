ef02c40cbe9cedd3b00da4a40a8d3343b5c35d24
Brent Abruzese <brentabruzese@Brents-MBP.fios-router.home>
2022-05-25 09:19:01 -0400

    more

--- netlify/functions/get-tweets.js
+++ netlify/functions/get-tweets.js
@@ -1,34 +1,43 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 const handler = async function (event, context) {
+  let url =
+    "https://api.twitter.com/2/users/16892009/tweets?tweet.fields=text&user.fields=username,profile_image_url&media.fields=preview_image_url,url";
 
+  const testUrl = "https://api.twitter.com/2/users/16892009/tweets?expansions=author_id&tweet.fields=text&user.fields=created_at,name,username,profile_image_url,url&media.fields=preview_image_url,url"
   if (!context.clientContext && !context.clientContext.identity) {
     return {
       statusCode: 500,
       // Could be a custom message or object i.e. JSON.stringify(err)
       body: JSON.stringify({
-        msg: 'No identity instance detected. Did you enable it?',
+        msg: "No identity instance detected. Did you enable it?",
       }),
-    }
+    };
   }
-  
-  const apiKey = "AAAAAAAAAAAAAAAAAAAAAFLycgEAAAAAmkWteNs0IURxKkr7%2FWhOIk6Lslc%3DMy7TfunNzuCWplL3z0NK8eC57ooHQRVFN5r22YBETqe04BiMF9"
+
+  const { token } = event.queryStringParameters
+  if (token) {
+    url = url.concat(`&next_token=${token}`);
+  }
+
+  const apiKey =
+    "AAAAAAAAAAAAAAAAAAAAAFLycgEAAAAAmkWteNs0IURxKkr7%2FWhOIk6Lslc%3DMy7TfunNzuCWplL3z0NK8eC57ooHQRVFN5r22YBETqe04BiMF9";
   const headers = {
-    'Authorization': `Bearer ${apiKey}`
-  }
+    Authorization: `Bearer ${apiKey}`,
+  };
 
-    const response = await fetch(`https://api.twitter.com/2/users/16892009/tweets?tweet.fields=text&user.fields=username,profile_image_url&media.fields=preview_image_url,url`, { headers })
+  const response = await fetch(testUrl, { headers });
 
-    const res = await response.json()
-    
+  const res = await response.json();
+
   return {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
-    body: JSON.stringify(res)
-  }
-}
+    body: JSON.stringify(res),
+  };
+};
 
-module.exports = { handler }
+module.exports = { handler };
